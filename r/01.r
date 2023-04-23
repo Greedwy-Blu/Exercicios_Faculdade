d542733c5c3215743950990c67b762ccb1a8ccc7
Greedwy-Blu <greedwyblu@gmail.com>
2023-04-23 12:21:33 -0300

    update

---
+++ r/01.r
@@ -0,0 +1,10 @@
+messagem <- "hello world"
+print(messagem)
+
+install.packages("ggplot2")
+
+#Carregando o pacote ggplot2
+library(ggplot2)
+
+# ggplot 2 é um pacote de visualização de dados - utilizado para a criação de gráficos
+?ggplot2
